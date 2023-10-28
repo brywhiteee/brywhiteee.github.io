3af7cf79151786c3d3077826365575202e6ea972
brywhiteee <98794297+brywhiteee@users.noreply.github.com>
2023-10-28 19:57:45 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,85 @@
-let slideIndex = 0;
-showSlides();
 
-function showSlides() {
-  let i;
-  let slides = document.getElementsByClassName("slider-wrapper2");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+let search = document.getElementById("search");
+
+let hero = [];
+var heroid = 0;
+
+search.onkeyup = function () {
+    var searchname = search.value;
+    if (searchname !== "") {
+      fetch(
+        "https://superheroapi.com/api.php/ 6890022557727131	/search/" +
+          searchname.trim()
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+          function showhero() {
+            var heronames = data.results;
+            console.log(data.results);
+           
+            for (var i of heronames) {
+              var li = document.createElement("li");
+              li.innerHTML = i.name;
+              li.id = i.id;
+  
+              li.addEventListener("click", function () {
+                heroid = this.id;
+                console.log(heroid + "this is id");
+                loadDetails(heroid);
+               
+              });
+            }
+          }
+
+          showhero();
+        })
+        .catch((err) => console.log(err));
+    }
+  };
+  
+  function loadDetails(heroid) {
+    fetch(`https://superheroapi.com/api.php/ 6890022557727131 /${heroid}`)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+  
+        var details = document.getElementById('details');
+        details.setAttribute("style","background-color:rgba(0,0,0,0.8);")
+  
+        var img = document.getElementById("img");
+        img.setAttribute("src", data.image.url);
+  
+        var name = document.getElementById("name");
+        name.innerHTML = data.name;
+  
+        var bio = document.getElementById("bio");
+        bio.innerHTML =   " Relatives :" +  data.connections.relatives ;
+  
+          var good = document.getElementById("good");
+          good.innerText = "Nature :" + data.biography.alignment;
+  
+        var base = document.getElementById("base");
+        base.innerHTML = "Work :" + data.work.base;
+  
+        var occ = document.getElementById("occupation");
+        occ.innerHTML = "Occupation :" + data.work.occupation;
+  
+        var powestat = document.getElementById("powerstats");
+        powestat.innerHTML =
+          "Intelligence : " +
+          data.powerstats.intelligence +
+          ", Combat : " +
+          data.powerstats.combat +
+          ", Power : " +
+          data.powerstats.power +
+          ", Speed : " +
+          data.powerstats.speed +
+          ", Strength : " +
+          data.powerstats.strength;
+  
+      })
+      .catch((error) => console.log(error));
   }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
-}
+  
+  
